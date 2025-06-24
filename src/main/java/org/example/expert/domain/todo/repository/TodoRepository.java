9a50e5b1987dc17c5d1b07e5e02cbd58cbb535ad
이준영 <tera199810@gmail.com>
2025-06-24 12:45:00 +0900

    fix: 필요없는 임포트 제거

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,16 +1,12 @@
 package org.example.expert.domain.todo.repository;
 
-import org.example.expert.domain.todo.dto.request.TodoGetRequest;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
