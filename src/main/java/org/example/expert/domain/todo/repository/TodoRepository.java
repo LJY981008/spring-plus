2c725fc5c4609a6176a0980f82f58e50400ad89b
이준영 <tera199810@gmail.com>
2025-06-24 11:42:00 +0900

    fix: Lv1-3 todos api 조건추가 TodoGetRequest를 통해 추가적인 정보 요청 TodoRepository에서 그에 따른 조건 jpql 작성

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.todo.repository;
 
+import org.example.expert.domain.todo.dto.request.TodoGetRequest;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,12 +8,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE t.weather LIKE '%' || :weather || '%'" +
+            "AND t.modifiedAt >= :startDate AND t.modifiedAt <= :endDate " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable, String weather, LocalDateTime startDate, LocalDateTime endDate);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
